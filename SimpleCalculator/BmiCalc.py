e9577b663a741e086a1a749354092c46706cd5c5
Gift Nestah <94760907+PantheraNestah@users.noreply.github.com>
2023-10-06 20:47:18 +0300

    Create BmiCalc.py

---
+++ SimpleCalculator/BmiCalc.py
@@ -0,0 +1,12 @@
+def func_bmiCalc():
+  float(weight) = input("Enter weight :")
+  float(height) = input("Enter height :") 
+  float(bmi) = weight / (height * height)
+  if (bmi < 24.0):
+    print("BMI below normal")
+  elif (bmi > 24.0):
+    print("BMI above normal") 
+  else:
+    print("BMI normal")
+
+func_bmiCalc()
