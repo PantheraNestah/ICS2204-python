59143e84bd05d1127ac9b81e2c7433fb46aebb36
PantheraNestah <pantheranestah@gmail.com>
2023-11-10 07:45:21 +0300

    added an instance object of the Calculator class

--- OOP/oop-calc.py
+++ OOP/oop-calc.py
@@ -1,13 +1,15 @@
+# SCT211-0071/2022 Gift Nestah
+
 class Calculator:
-    def __init(self, num1, num2):
+    def __init__(self, num1, num2):
         self.num1 = num1
         self.num2 = num2
 
-    def func_add():
+    def func_add(self):
         sum = self.num1 + self.num2
         print (f"The sum of {self.num1} and {self.num2} is: {sum}")
 
-    def func_ageCalc():
+    def func_ageCalc(self):
         year = int(input("Enter your year of birth: "))
         age_years = 2023 - year
         age_months = age_years * 12
@@ -16,3 +18,7 @@ class Calculator:
         print(f"You are {age_years} years old")
         print(f"You are {age_months} months old")
         print(f"You are approximately {age_days} days old")
+
+calc1 = Calculator(13, 43)
+calc1.func_add()
+calc1.func_ageCalc()
